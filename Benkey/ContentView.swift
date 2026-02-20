d51feb9c6f98ba5d48352a6357fa81e85f01da03
houquanchen <houquanchen@example.com>
2026-02-20 17:44:13 +0800

    Initial commit

---
+++ Benkey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Benkey
+//
+//  Created by houquan chen on 2026/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
